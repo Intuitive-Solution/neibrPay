b469c4595f67123d57203dbbb27e82f167e5b4a0
Tahir-Imran <timran@stratacache.com>
2025-11-08 20:37:30 +0530

    Update VerifyCsrfToken.php

--- backend/laravel/app/Http/Middleware/VerifyCsrfToken.php
+++ backend/laravel/app/Http/Middleware/VerifyCsrfToken.php
@@ -21,4 +21,25 @@ class VerifyCsrfToken extends Middleware
         'api/auth/magic-link',
         'api/auth/google/signup',
     ];
+
+    /**
+     * Determine if the request has a URI that should pass through CSRF verification.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    protected function inExceptArray($request)
+    {
+        foreach ($this->except as $except) {
+            if ($except !== '/') {
+                $except = trim($except, '/');
+            }
+
+            if ($request->is($except)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
