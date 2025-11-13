57c26d19a52c10f8a37b8e21105e01e2f36164ae
Tahir-Imran <timran@stratacache.com>
2025-11-13 19:05:54 +0530

    changed the label

--- apps/admin-web/src/views/Invoices.vue
+++ apps/admin-web/src/views/Invoices.vue
@@ -773,7 +773,7 @@
                                 d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
                               />
                             </svg>
-                            Duplicate
+                            Clone
                           </button>
                         </template>
                       </template>
@@ -883,7 +883,7 @@
                                 d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
                               />
                             </svg>
-                            Duplicate
+                            Clone
                           </button>
                           <div class="border-t border-gray-200 my-1"></div>
                           <button
